498ff22591dd77f3e4bb73a43186b7fda896103c
whydoesthecodenotwork <lawrencel54@nycstudents.net>
2024-03-06 12:47:24 -0500

    ow

--- parrot/src/components/BarChart.vue
+++ parrot/src/components/BarChart.vue
@@ -3,48 +3,40 @@
 </template>
 
 <script>
-import { Bar } from 'vue-chartjs'
-import {
-  Chart as ChartJS,
-  Title,
-  Tooltip,
-  Legend,
-  BarElement,
-  CategoryScale,
-  LinearScale
-} from 'chart.js'
+import { Bar } from "vue-chartjs";
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js";
 
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
 export default {
-  name: 'BarChart',
+  name: "BarChart",
   components: { Bar },
   data() {
     return {
       chartData: {
-        labels: ['lawrence', 'tian lang chen', 'jay'],
+        labels: ["lawrence", "tian lang chen", "jay"],
         datasets: [
           {
             data: [1, 200, 2],
-            label: 'jump height in cm',
+            label: "jump height in cm",
             borderWidth: 2,
-            borderColor: 'red',
-            backgroundColor: '#ff0000aa'
-          }
-        ]
+            borderColor: "red",
+            backgroundColor: "#ff0000aa",
+          },
+        ],
       },
       chartOptions: {
-        responsive: true
+        responsive: true,
       },
       plugins: {
         title: {
           display: true,
-          text: 'jump height'
-        }
-      }
-    }
-  }
-}
+          text: "jump height",
+        },
+      },
+    };
+  },
+};
 </script>
 
 <style scoped></style>
