5ead3a5511eafbe63d92e1c8b6f34d07b7ac868f
Keasalte <112724822+Keasalte@users.noreply.github.com>
2024-03-07 13:12:54 -0500

    doughnut is weirdge

---
+++ parrot/src/components/RaceChart.vue
@@ -0,0 +1,22 @@
+<template>
+    <main>
+        <Doughnut :data="data" :options="options" />
+    </main>
+  </template>
+  
+  <script>
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
+import { Doughnut } from 'vue-chartjs'
+
+
+ChartJS.register(ArcElement, Tooltip, Legend)
+
+export default {
+  name: 'App',
+  components: {
+    Doughnut
+  },
+}
+  </script>
+  
+  <style scoped></style>
---
+++ parrot/src/views/RaceView.vue
@@ -0,0 +1,28 @@
+<script setup>
+import RaceChart from "../components/RaceChart.vue";
+
+
+let babystuff = [];
+async function babystealing(){
+    try {
+        const response = await fetch('https://data.cityofnewyork.us/resource/wffy-3iyg.json?$query=SELECT%20birth_year%2C%20sex_of_infant%2C%20race_or_ethnicity_of_mother%2C%20births%2C%20percentage')
+        if (response.status != 200) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    babystuff.push(data);
+    console.log(babystuff);
+    } catch (error) {
+    }
+}
+
+babystealing();
+
+
+</script>
+
+<template>
+  <main>
+    <RaceChart :labels="names" :data="data" />
+  </main>
+</template>
