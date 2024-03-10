7996494db7b53bc8c42125285f98d5f722b1f15f
TLCChen <139132998+TLCChen@users.noreply.github.com>
2024-03-10 19:14:38 -0400

    3-10-24

--- parrot/src/components/TLCtesting.vue
+++ parrot/src/components/TLCtesting.vue
@@ -6,7 +6,8 @@
 let list2 = [];
 const valueList = new Set();
 let data = null;
-let babyObjet = [];
+let birthYear = [];
+let babiesBorn = [];
 async function getData() {
   const response = await fetch("https://data.cityofnewyork.us/resource/wffy-3iyg.json");
   data = await response.json();
@@ -15,23 +16,28 @@ async function getData() {
 }
 
 function getOccurrence(array, value) {
-  valueList.add(value);
-
   console.log(value, array.filter((v) => v === value).length);
-  console.log(valueList);
-  // babyObjet.push({ year: value, babies: array.filter((v) => v === value).length });
-  // console.log(babyObjet);
+  birthYear.push(value);
+  babiesBorn.push(array.filter((v) => v === value).length);
+}
+
+function createSet(array, set) {
+  set.add(array.birth_year);
 }
 
 function data2() {
-  data.forEach((sort) => getOccurrence(list2, sort.birth_year));
+  valueList.forEach((sort) => getOccurrence(list2, sort));
 }
 await getData();
+
+data.forEach((sort) => createSet(sort, valueList));
+console.log(valueList);
 data2();
+console.log(birthYear, babiesBorn);
 const list = [1, 2, 3, 4, 5];
 const cat = true;
 import { Bar } from "vue-chartjs";
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale, Colors } from "chart.js";
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from "chart.js";
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale);
 
@@ -41,8 +47,13 @@ export default {
   data() {
     return {
       chartData: {
-        labels: ["January", "February", "March"],
-        datasets: [{ data: [40, 20, 12] }],
+        labels: birthYear,
+        datasets: [
+          {
+            label: "Data Set",
+            data: babiesBorn,
+          },
+        ],
       },
       chartOptions: {
         responsive: true,
@@ -50,6 +61,20 @@ export default {
     };
   },
 };
+// export default {
+//   name: "BarChart",
+//   components: { Bar },
+//   props: {
+//     chartData: {
+//       type: Object,
+//       required: true,
+//     },
+//     chartOptions: {
+//       type: Object,
+//       default: () => {},
+//     },
+//   },
+// };
 </script>
 
 <style scoped></style>
--- parrot/src/views/HomeView.vue
+++ parrot/src/views/HomeView.vue
@@ -1,10 +1,19 @@
 <script setup>
 import BarChart from "../components/BarChart.vue";
 import TLCtesting from "@/components/TLCtesting.vue";
+const object1 = {
+  labels: ["January", "February", "March"],
+  datasets: [{ data: [40, 20, 45] }],
+};
+
+const object2 = {
+  responsive: true,
+};
 </script>
 
 <template>
   <main>
+    <!-- <TLCtesting :chartData="object1" :chartOptions="object2" /> -->
     <TLCtesting />
   </main>
 </template>
