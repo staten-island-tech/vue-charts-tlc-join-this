647dc8963372e7cde570f5d2a0bba1b47b55b545
dell <91681777+whydoesthecodenotwork@users.noreply.github.com>
2024-03-11 09:22:01 -0400

    bogdan code review
    the code is code

--- parrot/src/views/BabyNames.vue
+++ parrot/src/views/BabyNames.vue
@@ -4,44 +4,21 @@
     <div id="controls" @change="filter">
       <label>
         select a race
-        <select
-          id="ethcty"
-          :ref="
-            (el) => {
-              filterElements.race = el;
-            }
-          "
-        >
-          <option v-for="race in races" :key="race">{{ race }}</option>
+        <select id="ethcty" v-model="filters.ethcty">
+          <option v-for="race in options.ethcty" :key="race">{{ race }}</option>
           <option>all</option>
         </select>
       </label>
       <label>
         select a gender
-        <select
-          id="gndr"
-          :ref="
-            (el) => {
-              filterElements.gender = el;
-            }
-          "
-        >
-          <option v-for="gender in genders" :key="gender">{{ gender }}</option>
+        <select id="gndr" v-model="filters.gndr">
+          <option v-for="gender in options.gndr" :key="gender">{{ gender }}</option>
           <option>all</option>
         </select>
       </label>
       <label>
         year
-        <input
-          type="number"
-          id="brth_yr"
-          :ref="
-            (el) => {
-              filterElements.birthYear = el;
-            }
-          "
-          value="2015"
-        />
+        <input type="number" id="brth_yr" v-model="filters.brth_yr" value="2015" />
       </label>
     </div>
     <code v-if="loaded">{{ babies }}</code>
@@ -52,16 +29,29 @@
 <script setup>
 import { onMounted, ref, reactive } from "vue";
 const loaded = ref(false);
-const races = ref([]);
-const genders = ref([]);
-const birthYears = ref([]);
+let cache = {};
 let data = [];
 const babies = ref(null);
 
-const filterElements = reactive({});
+const options = reactive({
+  brth_yr: [],
+  ethcty: [],
+  gndr: [],
+});
+
+const filters = reactive({
+  brth_yr: "2015",
+  ethcty: "all",
+  gndr: "all",
+});
 
 async function getBabiesOfYear(year) {
   loaded.value = false;
+  if (cache.hasOwnProperty(year)) {
+    loaded.value = true;
+    return cache[year];
+  }
+
   try {
     const fData = await fetch(`https://data.cityofnewyork.us/resource/25th-nujf.json?brth_yr=${year}&$limit=50000&$$app_token=vPn84B6xK76CNcZ4XvQH3Oz0j`);
     const jData = await fData.json();
@@ -69,6 +59,8 @@ async function getBabiesOfYear(year) {
       throw new Error(`api error :( - ${jData.message}`);
     }
     loaded.value = true;
+    cache[year] = jData;
+    console.log(jData);
     return jData;
   } catch (e) {
     // probably should have better error handling
@@ -78,42 +70,37 @@ async function getBabiesOfYear(year) {
 
 async function refetch() {
   console.log("reloading");
-  data = await getBabiesOfYear(filterElements.birthYear.value);
+  data = await getBabiesOfYear(filters.brth_yr);
   babies.value = data;
 }
 
 // get all possible values of a column
 function getValuesOfColumn(column, data) {
-  return new Set(
-    data.map((baby) => {
-      return baby[column];
-    })
-  );
+  // sort by alphabet + remove duplicates
+  return new Set(data.map((baby) => baby[column]).sort());
 }
 
 async function filter() {
   await refetch();
-  Object.values(filterElements).forEach((filterElement) => {
-    console.log(filterElement);
-    data = filterBy(data, filterElement.id, filterElement.value);
-  });
+  console.log([filters]);
+  for (const key in filters) {
+    data = filterBy(data, key, filters[key]);
+  }
   babies.value = data;
 }
 
 function filterBy(data, property, value) {
-  if (value == "all") {
-    return data;
-  }
-  return data.filter((baby) => {
-    return baby[property] === value;
-  });
+  if (value === "all") return data;
+
+  console.log(property);
+  return data.filter((baby) => String(baby[property]) === String(value));
 }
 
 onMounted(async () => {
   data = await getBabiesOfYear(2015);
-  races.value = getValuesOfColumn("ethcty", data);
-  genders.value = getValuesOfColumn("gndr", data);
-  birthYears.value = getValuesOfColumn("brth_yr", data);
+  options.ethcty = getValuesOfColumn("ethcty", data);
+  options.gndr = getValuesOfColumn("gndr", data);
+  options.brth_yr = getValuesOfColumn("brth_yr", data);
   // evil ternary strikes again
   babies.value = data.length ? data : "there is no data...";
 });
