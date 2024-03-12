88789494dc0c2c25aa3a972b5140ca2877fbd0f9
Keasalte <jaychen1347@gmail.com>
2024-03-12 19:06:57 -0400

    HowtoOptionGroup

--- parrot/src/components/RaceChart.vue
+++ parrot/src/components/RaceChart.vue
@@ -5,39 +5,92 @@
 </template>
 
 <script>
+
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
 import { Doughnut } from "vue-chartjs";
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-export default {
-  props: ([raceName], [raceData]),
-  created() {
-    console.log(this.racelabels, this.racedata);
-  },
-  name: "PieChart",
-  components: {
-    Doughnut,
-  },
-  // https://vuejs.org/guide/components/props.html options api
-  data() {
-    return {
-      chartData: {
-        labels: this.raceName,
-        datasets: [
-          {
-            label: "cool",
-            data: this.raceData,
-            backgroundColor: ["rgb(255, 99, 132)", "rgb(54, 162, 235)", "rgb(255, 205, 86)"],
-            hoverOffset: 4,
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-      },
-    };
-  },
-};
+let babystuff2 = undefined;
+let babystuff = undefined;
+async function babystealing() {
+  try {
+    const response = await fetch(
+      "https://data.cityofnewyork.us/resource/wffy-3iyg.json"
+    );
+    if (response.status != 200) {
+      throw new Error(response.statusText);
+    }
+    const data = await response.json();
+    babystuff2 = data;
+  } catch (error) {}
+}
+
+async function filtering(){
+  await babystealing();
+  babystuff = babystuff2.filter((baby) => baby.birth_year === '2007')
+}
+
+  const raceData = {}
+  let races = [];
+
+  await filtering();
+  console.log(babystuff);
+  
+  babystuff.forEach((baby) => {
+    // if it doesn't already exist, create empty array
+    raceData[baby.race_or_ethnicity_of_mother] ??= [];
+    // add baby
+    raceData[baby.race_or_ethnicity_of_mother].push(baby)
+  });
+  console.log(raceData);
+  const a = new Array(6); //new Empty array
+  a.fill(0)
+  console.log(a); 
+
+  races = Object.keys(raceData)
+
+  // this goes through all races and adds to respective race
+  // chart takes array like labels=["white",.....] and data=[123879823,....].
+  // then chart will say "ok white babies have 123879823 births"
+  babystuff.forEach((baby) => {
+    a[races.indexOf(baby.race_or_ethnicity_of_mother)] += Number(baby.births);    // births was a STRING
+  })
+  console.log(races);
+  console.log(a);
+
+  export default {
+    name: "byRace",
+    components: { Doughnut },
+    data(){
+      return{
+        chartData: {
+          labels: ['White', 'Other', 'Hispanic', 'Black', 'Asian / Pacific Islander', 'Unknown',],
+          datasets: [
+            {
+              label: "Number of babies born",
+              backgroundColor:['#1b1c6b', '#1e0f53', '#2539bd', '#5055b6', '#848bf2', '#a6b0fe'],
+              data: a,
+              hoverOffset: 4
+            },
+          ],
+        },
+        chartOptions: {
+          responsive: true,
+        plugins: {
+          title: {
+            display: true,
+            text: "Babies sorted by race. 2017 to 2020"
+          }
+        }
+      }
+      }
+    }
+  }
+
 </script>
 
-<style scoped></style>
+<style scoped>
+*{
+  background-color: aliceblue;
+}
+</style>
--- parrot/src/views/RaceView.vue
+++ parrot/src/views/RaceView.vue
@@ -1,66 +1,39 @@
 <script setup>
-import { onMounted } from "vue";
 import RaceChart from "../components/RaceChart.vue";
 
-let babystuff = undefined;
-async function babystealing() {
-  try {
-    const response = await fetch("https://data.cityofnewyork.us/resource/wffy-3iyg.json");
-    if (response.status != 200) {
-      throw new Error(response.statusText);
-    }
-    const data = await response.json();
-    babystuff = data;
-  } catch (error) {}
-}
 
-const raceData = {};
-let races = [];
-
-onMounted(async () => {
-  await babystealing();
-  console.log(babystuff);
-
-  babystuff.forEach((baby) => {
-    // if it doesn't already exist, create empty arrayb
-    raceData[baby.race_or_ethnicity_of_mother] ??= [];
-    // add baby
-    raceData[baby.race_or_ethnicity_of_mother].push(baby);
-  });
-  console.log(raceData);
-
-  races = Object.keys(raceData);
-  const a = new Array(6); //new Empty array
-  a.fill(0);
-  console.log(a);
-  // this goes through all races and adds to respective race
-  // chart takes array like labels=["white",.....] and data=[123879823,....].
-  // then chart will say "ok white babies have 123879823 births"
-  babystuff.forEach((baby) => {
-    a[races.indexOf(baby.race_or_ethnicity_of_mother)] += Number(baby.births); // births was a STRING
-  });
-  console.log(races);
-  console.log(a);
-});
 </script>
 
 <template>
   <main>
-    <!-- PUT PROPERTIES HERE AFTER DEFINING IN RaceChart.vue -->
-    <RaceChart />
-
-    <form action="/action_page.php">
+    <RaceChart/>
+    <form>
       <label for="pies">Choose a filter:</label>
       <select name="pies" id="pies">
-        <optgroup label="Race">
-          <option value="White">White</option>
-          <option value="Black">Black</option>
-          <option value="Asian">Asian/Pacific Islander</option>
-          <option value="Hispanic">Hispanic</option>
-          <option value="Other">Other</option>
-          <option value="Unknown">Unknown</option>
+        <optgroup label="Year" id="Filling">
+          <option value="nuhuh">No Filter</option>
+          <option value="2007">2007</option>
+          <option value="2008">2008</option>
+          <option value="2009">2009</option>
+          <option value="2010">2010</option>
+          <option value="2011">2011</option>
+          <option value="2012">2012</option>
+          <option value="2013">2013</option>
+          <option value="2014">2014</option>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
+          <option value="2020">2020</option>
         </optgroup>
       </select>
     </form>
   </main>
 </template>
+
+<style scoped>
+label {
+  margin: 10px;
+}
+</style>
