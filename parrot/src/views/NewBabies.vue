fb9b6075e8c1ab910fb050acc5cf403b458159b8
TLCChen <tianlangchen0@gmail.com>
2024-03-21 13:23:15 -0400

    3-21-24

---
+++ parrot/src/views/NewBabies.vue
@@ -0,0 +1,18 @@
+<script setup>
+import BabyYear from "@/views/Newbars.vue";
+
+const object1 = {
+  labels: ["January", "February", "March"],
+  datasets: [{ data: [40, 20, 45] }],
+};
+
+const object2 = {
+  responsive: true,
+};
+</script>
+
+<template>
+  <main>
+    <BabyYear :chartData="object1" :chartOptions="object2" />
+  </main>
+</template>
