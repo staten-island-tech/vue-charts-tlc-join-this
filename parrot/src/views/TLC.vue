661e9baf8bc971627d21a4378e96e25a2c2cbf93
TLCChen <tianlangchen0@gmail.com>
2024-03-28 12:53:15 -0400

    Update TLC.vue

--- parrot/src/views/TLC.vue
+++ parrot/src/views/TLC.vue
@@ -63,6 +63,15 @@ async function createAll() {
       {
         data: babiesBorn,
         label: "Babies Born By Year",
+        backgroundColor: [
+          "rgb(255, 99, 132)",
+          "rgb(255, 159, 64)",
+          "rgb(255, 205, 86)",
+          "rgb(75, 192, 192)",
+          "rgb(54, 162, 235)",
+          "rgb(153, 102, 255)",
+          "rgb(201, 203, 207)",
+        ],
       },
     ],
   };
